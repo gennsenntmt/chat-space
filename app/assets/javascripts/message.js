b02210375297a5c4aed205894518bf14a9fea8e9
gennsenntmt <56209451+gennsenntmt@users.noreply.github.com>
2019-10-24 11:30:23 +0900

    complete

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,29 +3,29 @@ $(function(){
     var image = ""
     message.image ? image =`<img src="${message.image}">` : image =""
 
-    var buildMessageHTML = function(message) {
-        var html = `<div class="message" data-message-id="${message.id}">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-              ${image}
-          </div>
-        </div>`
+    // var buildMessageHTML = function(message) {
+    //     var html = `<div class="message" data-message-id="${message.id}">
+    //       <div class="upper-message">
+    //         <div class="upper-message__user-name">
+    //           ${message.user_name}
+    //         </div>
+    //         <div class="upper-message__date">
+    //           ${message.created_at}
+    //         </div>
+    //       </div>
+    //       <div class="lower-message">
+    //         <p class="lower-message__content">
+    //           ${message.content}
+    //         </p>
+    //           ${image}
+    //       </div>
+    //     </div>`
       
-      return html;
-    };
+    //   return html;
+    // };
   
     var html =  `
-                  <div class="message">
+                  <div class="message" data-id="${message.id}">
                     <div class="upper-message">
                       <div class="upper-message__user-name"> 
                         ${message.user_name}
@@ -46,7 +46,6 @@ $(function(){
                   `
                  return html;
   };
-
   $("#new_message").on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -62,36 +61,37 @@ $(function(){
     .done(function(message){
       var html = buildHTML(message);
       $('.messages').append(html)
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight })
       $('form').get(0).reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight })
       $('input').prop('disabled', false);
     })
     .fail(function(){
-      alert('ユーザー検索に失敗しました');
+      alert('メッセージを入力してください');
     });
   });
 
   var reloadMessages = function() {
     if(window.location.href.match(/\/groups\/\d+\/messages/)){ 
-      var last_message_id = $('.message:last').data('message-id');
+      var last_message_id = $('.message:last').data('id');
     $.ajax({
       url: "api/messages",
       type: 'get',
       dataType: 'json',
-      data: {last_id: last_message_id}
+      data: {id: last_message_id}
     })
     .done(function(messages) {
-     var insertHTML = '';
+      var insertHTML = '';
       messages.forEach(function(message){
         insertHTML = buildHTML(message);
         $('.messages').append(insertHTML);
+        
       })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
     .fail(function() {
       alert('自動更新に失敗しました');
     });
-    };
+    }
   };
   setInterval(reloadMessages, 5000);
 });
